d050083151c1b989dc75c7ee421e2d765e74dc5f
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 11:06:20 -0400

    Deploying updates to Heroku

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 import random
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fnafsmash.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL") or "sqlite:///fnafsmash.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
